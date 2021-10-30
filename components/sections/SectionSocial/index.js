3c786047f3d2b65d3fe27c536998b5658f87ef80
Vincenzo <vincenzorm117@gmail.com>
2021-10-30 18:23:38 -0400

    Setup social section component

---
+++ components/sections/SectionSocial/index.js
@@ -0,0 +1,8 @@
+import SocialLinks from "components/molecules/SocialLinks";
+
+const SectionSocial = ({}) => (
+  <section className="bg-gray-darkest text-white-dark flex flex-row justify-center py-10">
+    <SocialLinks />
+  </section>
+);
+export default SectionSocial;
