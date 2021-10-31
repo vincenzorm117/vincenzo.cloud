51cf8c9d0e824ba0fb09f914bc2737efed99e7b1
Vincenzo <vincenzorm117@gmail.com>
2021-10-31 09:55:32 -0400

    Setup education section

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -1,9 +1,11 @@
+import SectionEducation from "@/components/sections/SectionEducation";
 import SectionExperience from "@/components/sections/SectionExperience";
 
 export default function Home() {
   return (
     <>
       <SectionExperience />
+      <SectionEducation />
     </>
   );
 }
