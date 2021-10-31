83fc08af68f04c6390b5a112fbd9f1ab238883f2
Vincenzo <vincenzorm117@gmail.com>
2021-10-31 10:06:18 -0400

    Renamed component

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -2,7 +2,7 @@ import SectionEducation from "@/components/sections/SectionEducation";
 import SectionExperience from "@/components/sections/SectionExperience";
 import SectionUniversityExperience from "@/components/sections/SectionUniversityExperience";
 
-export default function Home() {
+export default function Page() {
   return (
     <>
       <SectionExperience />
