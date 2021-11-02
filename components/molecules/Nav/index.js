c2a354f9a4abd78b7dfdb07364bdfa11cb54ce0c
Vincenzo <vincenzorm117@gmail.com>
2021-11-01 23:28:19 -0400

    Updated nav to close on clicking menu item

--- components/molecules/Nav/index.js
+++ components/molecules/Nav/index.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import cn from "classnames";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import { useGlobalContext } from "providers/GlobalProvider";
+import { useRouter } from "next/dist/client/router";
 
 const links = [
   { text: "Home", href: "/" },
@@ -15,6 +16,19 @@ const Nav = ({}) => {
   const [isOpen, setIsOpen] = useState(false);
   const { maskHook } = useGlobalContext();
   const [, setMaskIsOpen] = maskHook;
+  const router = useRouter();
+
+  const toggleNav = useCallback(
+    (value) => {
+      setIsOpen(value);
+      setMaskIsOpen(value);
+    },
+    [setIsOpen, setMaskIsOpen]
+  );
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => toggleNav(false));
+  }, [router, toggleNav]);
 
   return (
     <nav className={cn("fixed z-20 bottom-0 right-0 pb-[25px] pr-[25px]")}>
@@ -29,15 +43,14 @@ const Nav = ({}) => {
             className="text-[20px] py-2 uppercase font-display font-bold text-white-ce tracking-widest"
             key={link.text}
           >
-            <Link href={link.href}>{link.text}</Link>
+            <Link href={link.href} onClick={() => toggleNav(false)}>
+              {link.text}
+            </Link>
           </li>
         ))}
       </ul>
       <button
-        onClick={() => {
-          setIsOpen(!isOpen);
-          setMaskIsOpen(!isOpen);
-        }}
+        onClick={() => toggleNav(!isOpen)}
         className="block ml-auto rounded-full bg-cyan-default shadow-md hover:shadow-lg transition-shadow duration-200 relative h-[50px] w-[50px]"
       >
         <div className={isOpen ? styles.burgerActive : styles.burger}></div>
