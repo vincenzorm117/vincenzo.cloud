791bdf3ae2cb911af2687caa1f0c9e55eef0c73a
Vincenzo <vincenzorm117@gmail.com>
2021-11-07 18:15:26 -0500

    Updated tf remote back to the way it was

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket               = "jumper-terraform-state"
-    workspace_key_prefix = var.domain
+    workspace_key_prefix = "vincenzo.cloud"
     key                  = "terraform.tfstate"
     region               = "us-east-1"
     encrypt              = true
