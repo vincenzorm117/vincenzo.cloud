3e25bd66bdbc586604f0f8e75f37ead96653e5f2
Vincenzo <vincenzorm117@gmail.com>
2021-11-20 17:32:39 -0500

    Updated NotionCallout component to include an emoji

--- site/components/notion-components/NotionCallout/index.js
+++ site/components/notion-components/NotionCallout/index.js
@@ -2,8 +2,11 @@ import NotionText from '../NotionText'
 
 const NotionCallout = (block) => {
   return (
-    <div className='pl-4 py-4 bg-[#fafafa] my-5'>
-      <NotionText field={block?.callout?.text} />
+    <div className='pl-4 py-4 bg-[#fafafa] my-5 flex'>
+      <div className='pr-4'>{block?.callout?.icon?.emoji}</div>
+      <p>
+        <NotionText field={block?.callout?.text} />
+      </p>
     </div>
   )
 }
