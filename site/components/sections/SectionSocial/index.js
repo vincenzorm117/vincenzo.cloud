3348509b40656304725ea44ccefe92a763e7054a
Vincenzo <vincenzorm117@gmail.com>
2021-11-22 11:25:07 -0500

    Setup darkmode on social bar

--- site/components/sections/SectionSocial/index.js
+++ site/components/sections/SectionSocial/index.js
@@ -1,7 +1,7 @@
 import SocialLinks from 'components/molecules/SocialLinks'
 
 const SectionSocial = ({}) => (
-  <section className='bg-gray-darkest text-white-dark flex flex-row justify-center py-10'>
+  <section className='text-gray-darkest bg-white-dark dark:bg-gray-darkest dark:text-white-dark flex flex-row justify-center py-10'>
     <SocialLinks />
   </section>
 )
