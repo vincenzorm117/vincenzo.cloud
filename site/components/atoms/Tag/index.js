05e1d20f28199daca9013ebfeb4c8437c2ca9025
Vincenzo <vincenzorm117@gmail.com>
2021-11-22 12:39:50 -0500

    Updated the Tag component to pull from the cascading currentColor

--- site/components/atoms/Tag/index.js
+++ site/components/atoms/Tag/index.js
@@ -1,18 +1,10 @@
 import cn from 'classnames'
 
-const Tag = ({ color = 'green', className = '', children = null }) => {
-  let styles = ''
-
-  switch (color) {
-    default:
-      styles = 'border-green-grey text-green-grey'
-  }
-
+const Tag = ({ className = '', children = null }) => {
   return (
     <span
       className={cn(
-        'inline-block py-[4px] px-[9px] bg-transparent border-[1px] rounded-md border-green-grey border-solid leading-[16px] m-1',
-        styles,
+        'inline-block py-[4px] px-[9px] bg-transparent border-[1px] rounded-md border-[currentColor] border-solid leading-[16px] m-1',
         className
       )}
     >
