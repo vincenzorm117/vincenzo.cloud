b28e1720b576a3e67ed1687bb4cf0b5d6f91efe3
Vincenzo <vincenzorm117@gmail.com>
2022-07-17 15:57:00 -0600

    Installed storybook

--- site-new/pages/index.js
+++ site-new/pages/index.js
@@ -33,16 +33,14 @@ const Home = () => (
 
         <a
           href='https://github.com/vercel/next.js/tree/canary/examples'
-          className={styles.card}
-        >
+          className={styles.card}>
           <h2>Examples &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
 
         <a
           href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          className={styles.card}
-        >
+          className={styles.card}>
           <h2>Deploy &rarr;</h2>
           <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </a>
@@ -53,8 +51,7 @@ const Home = () => (
       <a
         href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
         target='_blank'
-        rel='noopener noreferrer'
-      >
+        rel='noopener noreferrer'>
         Powered by{' '}
         <span className={styles.logo}>
           <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
