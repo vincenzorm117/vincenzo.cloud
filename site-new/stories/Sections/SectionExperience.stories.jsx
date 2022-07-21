ef633205289be9393e96f8f5a7598878c6af8622
Vincenzo <vincenzorm117@gmail.com>
2022-07-20 21:33:08 -0600

    Added new story

---
+++ site-new/stories/Sections/SectionExperience.stories.jsx
@@ -0,0 +1,11 @@
+import SectionExperience from '@components/sections/SectionExperience'
+
+export default {
+  title: 'sections/SectionExperience',
+  component: SectionExperience,
+}
+
+const Template = (args) => <SectionExperience {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {}
