21fba3d03ab96918f7235a44a049e192179166c2
Vincenzo <vincenzorm117@gmail.com>
2022-07-23 17:19:55 -0600

    Reorganized _app and _document code

--- site-new/pages/_app.js
+++ site-new/pages/_app.js
@@ -3,7 +3,6 @@ import Mask from 'components/atoms/Mask'
 import Nav from 'components/molecules/Nav'
 import Head from 'next/head'
 import { useEffect, useMemo, useReducer, useState } from 'react'
-// import 'tailwindcss/tailwind.css'
 import GlobalProvider from '../providers/GlobalProvider'
 import '../styles/_globals.scss'
 
@@ -38,51 +37,19 @@ const MyApp = ({ Component, pageProps }) => {
   }, [])
 
   return (
-    <GlobalProvider.Provider value={globalProviderValue}>
+    <>
       <Head>
-        <meta charSet='utf-8' />
-        <meta name='description' content="Vincenzo Marconi's Personal Site" />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Vincenzo</title>
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/favicon/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/favicon/site.webmanifest' />
-        <link
-          rel='mask-icon'
-          href='/favicon/safari-pinned-tab.svg'
-          color='#ec4747'
-        />
-        <meta name='msapplication-TileColor' content='#eeeeee' />
-        <meta name='theme-color' content='#ffffff' />
-        <link
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,700'
-          rel='stylesheet'
-        />
       </Head>
-      <main
-        style={{ minHeight: 'calc(100vh - 250px)' }}
-        className='flex flex-col dark:bg-purple-dark dark:text-white-default'>
-        <Component {...pageProps} />
-        <Nav />
-      </main>
-      <SectionFooter />
-      <Mask />
-    </GlobalProvider.Provider>
+      <GlobalProvider.Provider value={globalProviderValue}>
+        <main className='flex flex-col dark:bg-purple-dark dark:text-white-default min-h-[calc(100vh_-_250px)]'>
+          <Component {...pageProps} />
+          <Nav />
+        </main>
+        <SectionFooter />
+        <Mask />
+      </GlobalProvider.Provider>
+    </>
   )
 }
 
--- site-new/pages/_document.js
+++ site-new/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,7 +9,40 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <meta charSet='utf-8' />
+          <meta name='description' content="Vincenzo Marconi's Personal Site" />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/favicon/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/favicon/site.webmanifest' />
+          <link
+            rel='mask-icon'
+            href='/favicon/safari-pinned-tab.svg'
+            color='#ec4747'
+          />
+          <meta name='msapplication-TileColor' content='#eeeeee' />
+          <meta name='theme-color' content='#ffffff' />
+          <link
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=optional'
+            rel='stylesheet'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
